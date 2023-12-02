1687f690908cc2f3ec890b5f655d37798ad2a7d2
sumondas2001 <sumonchandradas1010@gmail.com>
2023-12-03 03:16:44 +0600

    object

---
+++ object.js
@@ -0,0 +1,32 @@
+// var mobile = {
+//     name : 'realme',
+//     color : 'black',
+//     stroage : '128gb',
+//     price : 28000,
+//     camera : '65mp',
+//     processor : "750g"
+
+// }
+// console.log(mobile);
+// console.log(mobile.color);
+// console.log(mobile.price);
+// console.log(mobile.processor);
+// console.log(mobile.color);
+
+
+
+var myComputer = {
+    brand : "dell",
+    color : 'blsck',
+    display : '14 inc',
+    model : 'vostro NOtebook 3401',
+    hardDrive : '1Tb'
+}
+myComputer.hardDrive = '2Tb'
+console.log(myComputer);
+console.log(myComputer.hardDrive);
+console.log(myComputer.color);
+console.log(myComputer.model);
+console.log(myComputer.color);
+
+
